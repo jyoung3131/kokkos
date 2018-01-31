ce270979c8fe901bdeeb7a9d067addc83c6cb9c6
crtrott <crtrott@sandia.gov>
2018-01-31 12:44:08 -0700

    CilkPlus: Adding MDRange Cilk port

--- core/src/CilkPlus/Kokkos_CilkPlus_MDRange.hpp
+++ core/src/CilkPlus/Kokkos_CilkPlus_MDRange.hpp
@@ -1,43 +1,35 @@
+#ifndef KOKKOS_CILKPLUS_MDRANGE_HPP
+#define KOKKOS_CILKPLUS_MDRANGE_HPP
+
 #include<cilk/cilk.h>
-#include<cilk/reducer_opadd.h>
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-/* Parallel patterns for Kokkos::Experimental::CilkPlus with RangePolicy */
 
 namespace Kokkos {
 namespace Impl {
 
+
 template< class FunctorType , class ... Traits >
 class ParallelFor< FunctorType ,
-                   Kokkos::RangePolicy< Traits ... > ,
+                   Kokkos::MDRangePolicy< Traits ... > ,
                    Kokkos::Experimental::CilkPlus
                  >
 {
 private:
 
-  typedef Kokkos::RangePolicy< Traits ... > Policy ;
+  typedef Kokkos::MDRangePolicy< Traits ... > MDRangePolicy ;
+  typedef typename MDRangePolicy::impl_range_policy Policy ;
 
-  const FunctorType m_functor ;
-  const Policy      m_policy ;
+  typedef typename Kokkos::Impl::HostIterateTile< MDRangePolicy, FunctorType, typename MDRangePolicy::work_tag, void > iterate_type;
 
-  template< class TagType >
-  typename std::enable_if< std::is_same< TagType , void >::value >::type
+  const FunctorType   m_functor ;
+  const MDRangePolicy m_mdr_policy ;
+  const Policy        m_policy ;
+
+  void
   exec() const
     {
       const typename Policy::member_type e = m_policy.end();
       cilk_for ( typename Policy::member_type i = m_policy.begin() ; i < e ; ++i ) {
-        m_functor( i );
-      }
-    }
-
-  template< class TagType >
-  typename std::enable_if< ! std::is_same< TagType , void >::value >::type
-  exec() const
-    {
-      const TagType t{} ;
-      const typename Policy::member_type e = m_policy.end();
-      cilk_for ( typename Policy::member_type i = m_policy.begin() ; i < e ; ++i ) {
-        m_functor( t , i );
+        iterate_type( m_mdr_policy, m_functor )( i );
       }
     }
 
@@ -45,32 +37,36 @@ public:
 
   inline
   void execute() const
-    { this-> template exec< typename Policy::work_tag >(); }
+    { this->exec(); }
 
   inline
-  ParallelFor( const FunctorType & arg_functor
-             , const Policy      & arg_policy )
+  ParallelFor( const FunctorType   & arg_functor
+             , const MDRangePolicy & arg_policy )
     : m_functor( arg_functor )
-    , m_policy(  arg_policy )
+    , m_mdr_policy(  arg_policy )
+    , m_policy( Policy(0, m_mdr_policy.m_num_tiles).set_chunk_size(1) )
     {}
 };
 
 template< class FunctorType , class ReducerType , class ... Traits >
 class ParallelReduce< FunctorType
-                    , Kokkos::RangePolicy< Traits ... >
+                    , Kokkos::MDRangePolicy< Traits ... >
                     , ReducerType
                     , Kokkos::Experimental::CilkPlus
                     >
 {
 private:
 
-  typedef Kokkos::RangePolicy< Traits ... > Policy ;
-  typedef typename Policy::work_tag                                  WorkTag ;
+  typedef Kokkos::MDRangePolicy< Traits ... > MDRangePolicy ;
+  typedef typename MDRangePolicy::impl_range_policy Policy ;
+
+  typedef typename MDRangePolicy::work_tag                                  WorkTag ;
 
   typedef Kokkos::Impl::if_c< std::is_same<InvalidType,ReducerType>::value, FunctorType, ReducerType> ReducerConditional;
-
   typedef typename ReducerConditional::type ReducerTypeFwd;
   typedef typename Kokkos::Impl::if_c< std::is_same<InvalidType,ReducerType>::value, WorkTag, void>::type WorkTagFwd;
+
+  typedef typename ReducerTypeFwd::value_type ValueType;
 
   typedef FunctorAnalysis< FunctorPatternInterface::REDUCE , Policy , FunctorType > Analysis ;
 
@@ -79,14 +75,22 @@ private:
   typedef typename Analysis::pointer_type    pointer_type ;
   typedef typename Analysis::reference_type  reference_type ;
 
+
+  using iterate_type = typename Kokkos::Impl::HostIterateTile< MDRangePolicy
+                                                                           , FunctorType
+                                                                           , WorkTag
+                                                                           , ValueType
+                                                                           >;
+
+
   const FunctorType   m_functor ;
+  const MDRangePolicy m_mdr_policy ;
   const Policy        m_policy ;
   const ReducerType   m_reducer ;
   const pointer_type  m_result_ptr ;
 
-  template< class TagType >
   inline
-  typename std::enable_if< std::is_same< TagType , void >::value >::type
+  void
   exec( reference_type update ) const
     {
       static_assert(std::is_same<ReducerType,Kokkos::Experimental::Sum<typename Analysis::value_type>>::value ||
@@ -95,26 +99,7 @@ private:
       cilk::reducer< cilk::op_add<typename Analysis::value_type> > parallel_sum(0);
       cilk_for ( typename Policy::member_type i = m_policy.begin() ; i < e ; ++i ) {
         typename Analysis::value_type lupdate = 0;
-        m_functor( i , lupdate );
-        *parallel_sum += lupdate;
-      }
-      update = parallel_sum.get_value();
-    }
-
-  template< class TagType >
-  inline
-  typename std::enable_if< ! std::is_same< TagType , void >::value >::type
-  exec( reference_type update ) const
-    {
-      const TagType t{} ;
-
-      static_assert(std::is_same<ReducerType,Kokkos::Experimental::Sum<typename Analysis::value_type>>::value ||
-                    std::is_same<ReducerType,Kokkos::InvalidType>::value ,"Only Sum Reductions are supported by the CilkPlus execution space");
-      const typename Policy::member_type e = m_policy.end();
-      cilk::reducer< cilk::op_add<typename Analysis::value_type> > parallel_sum(0);
-      cilk_for ( typename Policy::member_type i = m_policy.begin() ; i < e ; ++i ) {
-        typename Analysis::value_type lupdate = 0;
-        m_functor( t, i , lupdate );
+        iterate_type( m_mdr_policy, m_functor, lupdate )( i );
         *parallel_sum += lupdate;
       }
       update = parallel_sum.get_value();
@@ -144,7 +129,7 @@ public:
       reference_type update =
         ValueInit::init(  ReducerConditional::select(m_functor , m_reducer) , ptr );
 
-      this-> template exec< WorkTag >( update );
+      this-> exec( update );
 
       Kokkos::Impl::FunctorFinal< ReducerTypeFwd , WorkTagFwd >::
         final(  ReducerConditional::select(m_functor , m_reducer) , ptr );
@@ -152,14 +137,15 @@ public:
 
   template< class HostViewType >
   ParallelReduce( const FunctorType  & arg_functor ,
-                  const Policy       & arg_policy ,
+                  const MDRangePolicy       & arg_policy ,
                   const HostViewType & arg_result_view ,
                   typename std::enable_if<
                                Kokkos::is_view< HostViewType >::value &&
                               !Kokkos::is_reducer_type<ReducerType>::value
                   ,void*>::type = NULL)
     : m_functor( arg_functor )
-    , m_policy( arg_policy )
+    , m_mdr_policy( arg_policy )
+    , m_policy( Policy(0, m_mdr_policy.m_num_tiles).set_chunk_size(1) )
     , m_reducer( InvalidType() )
     , m_result_ptr( arg_result_view.data() )
     {
@@ -169,12 +155,14 @@ public:
       static_assert( std::is_same< typename HostViewType::memory_space , HostSpace >::value
         , "Kokkos::Experimental::CilkPlus reduce result must be a View in HostSpace" );
     }
+
   inline
   ParallelReduce( const FunctorType & arg_functor
-                , Policy       arg_policy
+                , MDRangePolicy       arg_policy
                 , const ReducerType& reducer )
     : m_functor( arg_functor )
-    , m_policy(  arg_policy )
+    , m_mdr_policy(  arg_policy )
+    , m_policy( Policy(0, m_mdr_policy.m_num_tiles).set_chunk_size(1) )
     , m_reducer( reducer )
     , m_result_ptr(  reducer.view().data() )
     {
@@ -183,3 +171,4 @@ public:
 }
 }
 
+#endif
